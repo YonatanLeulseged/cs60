7d6698fb8ea3faa08ef47abb5196e697165c5e9e
YonatanLeulseged <41715443+YonatanLeulseged@users.noreply.github.com>
2020-03-15 20:13:02 -0700

    add printf

---
+++ lab1.c
@@ -0,0 +1,19 @@
+/*-------------------*/
+/* Yonatan Leulseged */
+/*	Lab 1        */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+
+   printf("\nLab1. \n\n");
+   printf("Hello World.\n");
+   printf("Hi, Yonatan Leulseged. \n\n");
+   printf("The world can't tell you who you are. You've just got\n");
+   printf("to figure out who you are and be there,for better or worse.\n-Dave Chappele\n\n");
+	
+  return EXIT_SUCCESS;
+
+}
+/*-------------------*/
